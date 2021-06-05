3e2717c5f0c588e7f351ddb6d72a9c82e46ddf1b
Tegar Oakley <tegaroakley@gmail.com>
2021-06-05 11:07:34 +0700

    layouting

--- app/src/main/java/com/example/utspraktik_if2_10118066_tegarluckyoakley/MainActivity.java
+++ app/src/main/java/com/example/utspraktik_if2_10118066_tegarluckyoakley/MainActivity.java
@@ -2,7 +2,17 @@ package com.example.utspraktik_if2_10118066_tegarluckyoakley;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +21,29 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-}
+
+    final Calendar myCalendar = Calendar.getInstance();
+
+    EditText edittext= (EditText) findViewById(R.id.TanggalLahir);
+    DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
+
+        @Override
+        public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+            myCalendar.set(Calendar.YEAR, year);
+            myCalendar.set(Calendar.MONTH, monthOfYear);
+            myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+            updateLabel();
+        }
+        };
+
+    private void updateLabel() {
+        String myFormat = "MM/dd/yy"; //In which you need put here
+        SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
+
+        edittext.setText(sdf.format(myCalendar.getTime()));
+    }
+    };
+
+
+
+
